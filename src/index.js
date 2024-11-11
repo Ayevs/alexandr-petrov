6642d6d1708d8319c452dd9c31526031b9a1a204
Alexandr <programalexandr@gmail.com>
2024-11-10 22:38:13 -0500

    reverting route path

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ root.render(
   <BrowserRouter basename="/alexandr-petrov">
     <Routes>
       <Route path="/" element={<App />} />
-      <Route path="alexandr-petrov/projects" element={<Projects />} />
+      <Route path="/projects" element={<Projects />} />
     </Routes>
   </BrowserRouter>
 );
